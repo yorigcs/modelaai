6a9fc2759f8edbe2052deb5d9de2e021717b0bf7
Yorí Galisteu <yorigcsdev@gmail.com>
2023-01-18 16:52:44 -0300

    chore: init spring boot project

---
+++ src/main/java/com/api/controllers/UserController.java
@@ -0,0 +1,4 @@
+package com.api.controllers;
+
+public class UserController {
+}
---
+++ src/test/java/com/api/modelaai/ModelaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.modelaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelaAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
